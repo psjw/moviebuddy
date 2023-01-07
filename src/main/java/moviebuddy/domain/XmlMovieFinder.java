94f1981708a9782a0fb810cd14b04365a1d44b11
psjw <dlrudtn108@gmail.com>
2023-01-07 20:41:30 +0900

    Step02

---
+++ src/main/java/moviebuddy/domain/XmlMovieFinder.java
@@ -0,0 +1,16 @@
+package moviebuddy.domain;
+
+import java.util.List;
+
+/**
+ * packageName : moviebuddy.domain
+ * fileName : XmlMovieFinder
+ * author : psjw
+ */
+public class XmlMovieFinder extends MovieFinder{
+
+    @Override
+    public List<Movie> loadMovies() {
+        return null;
+    }
+}
