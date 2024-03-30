6a1e077b281ad31db9090e2533eb6e9e0134d976
psjw <dlrudtn108@gmail.com>
2024-03-30 17:09:36 +0900

    Section08: Refactor testing with test-context framework

--- src/test/java/moviebuddy/domain/JaxbMovieReaderTest.java
+++ src/test/java/moviebuddy/domain/JaxbMovieReaderTest.java
@@ -1,16 +1,23 @@
 package moviebuddy.domain;
 
+import moviebuddy.MovieBuddyFactory;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {MovieBuddyFactory.class})
 public class JaxbMovieReaderTest {
-
+    @Autowired JaxbMovieReader movieReader;
 
     @Test
     void NotEmpty_LoadedMovies(){
-        JaxbMovieReader movieReader = new JaxbMovieReader();
+//        JaxbMovieReader movieReader = new JaxbMovieReader();
         List<Movie> movies = movieReader.loadMovies();
         //movies.size() //XML 문서에 등록된 영화 수와 동일한가?
 //        MovieFinderTest.assertEquals(1375, movies.size());
--- src/test/java/moviebuddy/domain/MovieFinderTest.java
+++ src/test/java/moviebuddy/domain/MovieFinderTest.java
@@ -3,18 +3,32 @@ package moviebuddy.domain;
 import moviebuddy.MovieBuddyFactory;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import java.util.List;
 
 /**
  * @author springrunner.kr@gmail.com
  */
+@SpringJUnitConfig(MovieBuddyFactory.class)
+//@ExtendWith(SpringExtension.class) //JUnit이 테스트 실행전략을 확장할때 사용 // SpringExtension에서 제공하는 스프링 컨테이너 제공 관리
+//@ContextConfiguration(classes = MovieBuddyFactory.class) //해당 Annotation이 지정한 스프링 구성정보를 바탕으로 실행
 public class MovieFinderTest {
-	final ApplicationContext applicationContext =
-			new AnnotationConfigApplicationContext(MovieBuddyFactory.class);
-	final MovieFinder movieFinder = applicationContext.getBean(MovieFinder.class);
+//	final MovieFinder movieFinder;
+//	@Autowired
+//	public MovieFinderTest(MovieFinder movieFinder) {
+//		this.movieFinder = movieFinder;
+//	}
+
+	@Autowired
+	MovieFinder movieFinder;
+
+//	MovieFinder movieFinder;
+//	@Autowired
+//	public void setMovieFinder(MovieFinder movieFinder) {
+//		this.movieFinder = movieFinder;
+//	}
 
 	@Test
 	void NotEmpty_directedBy(){
