cc2e3fb1c7d4d3b34d62794d55403c0a4f448c39
psjw <dlrudtn108@gmail.com>
2024-03-30 15:58:37 +0900

    Section06: Use java-based configuration to dependency injection

--- src/main/java/moviebuddy/MovieBuddyFactory.java
+++ src/main/java/moviebuddy/MovieBuddyFactory.java
@@ -2,14 +2,40 @@ package moviebuddy;
 
 import moviebuddy.domain.CsvMovieReader;
 import moviebuddy.domain.MovieFinder;
+import moviebuddy.domain.MovieReader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
+@Import({MovieBuddyFactory.DomainModuleConfig.class, MovieBuddyFactory.DataSourceModuleConfig.class})
+//XML의 경우
+//@ImportResource("xml file location")
 public class MovieBuddyFactory {
+    @Configuration
+    static class DomainModuleConfig {
+//    @Bean
+//    public MovieFinder movieFinder(){
+//        return new MovieFinder(movieReader());
+//    }
 
-    @Bean
-    public MovieFinder movieFinder(){
-        return new MovieFinder(new CsvMovieReader());
+        //빈스코드
+        //빈을 요청 할때마다 새로 생성 : 프로토타입 스코프
+        //단 하나의 빈을 생성 : 싱글톤 스코프
+        @Bean
+//    @Scope("prototype")
+//    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+        public MovieFinder movieFinder(MovieReader movieReader) {
+            return new MovieFinder(movieReader);
+        }
+
+    }
+
+    @Configuration
+    static class DataSourceModuleConfig {
+        @Bean
+        public MovieReader movieReader() {
+            return new CsvMovieReader();
+        }
     }
 }
